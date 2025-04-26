920868e93f5fdfd073280b5a0a943f11fdbf7a6e
Elif İrem KAYA <139874400+ElifiremKY@users.noreply.github.com>
2025-04-26 20:08:16 +0300

    Update ödev2-1.sayfa1.soru.kt

--- ödev2-1.sayfa1.soru.kt
+++ ödev2-1.sayfa1.soru.kt
@@ -1,1 +1,10 @@
+fun dereced(c: Double): Double {
+    return c * 1.8 + 32
+}
 
+fun main() {
+    println("bir sıcaklık değeri girin:")
+    val derece = readLine()!!.toDouble()
+    val fahrenheit = dereced(derece)
+    println("$derece derece = $fahrenheit Fahrenheit")
+}
